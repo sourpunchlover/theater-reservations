6817b38d22f1625054df4c2d0be7e692d6bc25e5
sourpunchlover <sndgpdesigns@gmail.com>
2026-04-14 13:24:37 -0400

    Added sout & string statements to collect user input.

--- src/main/java/com/pluralsight/TheaterReservations.java
+++ src/main/java/com/pluralsight/TheaterReservations.java
@@ -1,4 +1,18 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class TheaterReservations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // need a name, a date & # tickets
+        System.out.println("Welcome To Theater Reservations!\n");
+        System.out.print("Please enter your full name: ");
+        String fullName = scanner.nextLine().trim();
+        System.out.print("Enter date of show you are attending (MM/dd/yyy): ");
+        String showDate = scanner.nextLine().trim();
+        System.out.print("How many tickets are needed? ");
+        String numTickets = scanner.nextLine().trim();
+
+    }
 }
